037cf68afb8629c2992ab15f80ec2adcdc598915
Matthew Conde <matt.conde@gmail.com>
2016-07-21 11:46:31 +0100

    Yet another user color tweak

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -130,12 +130,12 @@ function prompt_user -d "Display current user if different from $default_user"
       else
         set USER_PROMPT $USER
       end
-      prompt_segment brmagenta black $USER_PROMPT
+      prompt_segment magenta black $USER_PROMPT
     end
   else
     get_hostname
     if [ $HOSTNAME_PROMPT ]
-      prompt_segment brmagenta black $HOSTNAME_PROMPT
+      prompt_segment magenta black $HOSTNAME_PROMPT
     end
   end
 end
